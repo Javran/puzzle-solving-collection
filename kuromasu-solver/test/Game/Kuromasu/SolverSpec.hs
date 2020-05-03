71f43742ef3de5efc33362c82384fbe781709cf0
Javran Cheng <Javran.C@gmail.com>
2020-05-02 23:54:03 -0700

    WIP: basic structure for testing.

--- kuromasu-solver/test/Game/Kuromasu/SolverSpec.hs
+++ kuromasu-solver/test/Game/Kuromasu/SolverSpec.hs
@@ -4,6 +4,7 @@ import Test.Hspec
 
 import Control.Monad.IO.Class
 import Game.Kuromasu.Parser
+import Control.Monad
 import Game.Kuromasu.Solver
 import Paths_kuromasu_solver
 import Data.Maybe
@@ -20,7 +21,52 @@ import qualified Data.List.Match as LMatch
 
   For now this issue isn't very important,
   but in future this is definitely worth pointing out explicitly.
+ -}
 
+exampleRaw0 :: [String]
+exampleRaw0 =
+  [ "9 9"
+  , "? ? 3 2 ? ? r 1 1"
+  , "r 4 ? r ? ? ? ? ?"
+  , "? ? ? ? ? ? 7 ? ?"
+  , "? ? ? ? 8 8 ? 9 8"
+  , "5 ? ? 6 ? ? ? ? 4"
+  , "5 r ? ? ? r 5 ? ?"
+  , "? ? 6 ? 3 ? ? ? r"
+  , "? r 5 ? r ? 4 r 1"
+  , "? ? ? 3 ? 5 ? 3 ?"
+  ]
+
+exampleRaw1 :: [String]
+exampleRaw1 =
+  [ "9 9"
+  , "1 ? 3 ? ? ? ? 3 ?"
+  , "? ? ? ? 4 4 ? ? r"
+  , "? 9 8 ? 8 ? ? ? ?"
+  , "2 ? ? ? ? ? ? ? 2"
+  , "? ? ? ? 2 ? ? 6 4"
+  , "? r ? 8 ? ? 8 ? 7"
+  , "2 ? ? ? ? r ? 4 ?"
+  , "? ? 5 ? ? ? ? ? 3"
+  , "? ? 7 ? ? ? r ? r"
+  ]
+
+exampleRaw2 :: [String]
+exampleRaw2 =
+  [ "9 9"
+  , "? r 2 r ? r ? 6 2"
+  , "r 4 ? 6 4 ? 2 ? ?"
+  , "r ? ? 7 ? ? r 5 ?"
+  , "? ? ? ? ? ? ? ? 7"
+  , "r 4 r ? r ? ? 6 6"
+  , "4 ? ? r 4 1 r ? ?"
+  , "? ? ? 6 ? ? ? ? ?"
+  , "? ? ? ? 7 ? 6 6 r"
+  , "4 5 ? 7 ? ? ? ? ?"
+  ]
+
+
+{-
   TODO: rules are (according to 0hn0 rather than the original game
   as described in wikipedia page):
 
@@ -29,9 +75,25 @@ import qualified Data.List.Match as LMatch
   - blue dots sees at least one.
     (in other words, blue dots cannot be surrounded by red dots).
  -}
-
-
 spec :: Spec
 spec =
-  describe "mkBoardFromRep" $
-    pure ()
+  describe "solve" $ do
+    let solveAndVerifyBoard bdRep = do
+          Just (bd, hints) <- pure $ mkBoardFromRep bdRep
+          Just solved <- pure $ extractAnswer (solve bd)
+          -- TODO: verify dimensions match
+          -- TODO: verify compatibility
+          -- TODO: blue dots number verify
+          pending
+        mkExample name rawInp =
+          specify name $ do
+            Just bdRep <- pure $ parseBoard (unlines rawInp)
+            solveAndVerifyBoard bdRep
+    mkExample "example0" exampleRaw0
+    mkExample "example1" exampleRaw1
+    mkExample "example2" exampleRaw2
+    specify "data/puzzles.txt" $ do
+      puzzlesFilePath <- getDataFileName "data/puzzles.txt"
+      content <- readFile puzzlesFilePath
+      let parsed = parseBoards content
+      mapM_ solveAndVerifyBoard parsed
