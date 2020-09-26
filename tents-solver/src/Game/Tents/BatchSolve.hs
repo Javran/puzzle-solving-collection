05ece83e26bc6572fd246b015c7816d9c147ce2d
Javran Cheng <Javran.C@gmail.com>
2020-09-26 16:05:01 -0700

    improve on batch-solve.

--- tents-solver/src/Game/Tents/BatchSolve.hs
+++ tents-solver/src/Game/Tents/BatchSolve.hs
@@ -1,11 +1,31 @@
 module Game.Tents.BatchSolve where
 
+import Control.Monad.Except
 import Game.Tents.Parser
+import Game.Tents.Solver
+import Game.Tents.Types
+
+m2e :: MonadError e m => e -> Maybe a -> m a
+m2e errMsg v = case v of
+  Nothing -> throwError errMsg
+  Just r -> pure r
+
+processBoard :: BoardRep -> ExceptT String IO ()
+processBoard br = do
+  bd0 <- m2e "Board failed to initialize." $ mkBoard br
+  bd1 <- m2e "Failure while solving." $ solve bd0
+  unless (isSolved bd1) $
+    throwError "Result is incomplete."
 
 batchSolve :: FilePath -> IO ()
 batchSolve fp = do
   raw <- readFile fp
   let boards = parseBatchBoards raw
   putStrLn $ "Parsed " <> show (length boards) <> " boards from this batch."
-  print boards
-  -- TODO: perform solving
+  forM_ boards $ \(bId, br) -> do
+    r <- runExceptT (processBoard br)
+    case r of
+      Left e ->
+        putStrLn $ "Board " <> bId <> " failed due to reason: " <> e
+      Right _ ->
+        putStrLn $ "Board " <> bId <> " is successfully solved."
