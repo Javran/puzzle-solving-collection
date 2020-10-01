882e9f0ae395008c3f1d4a8ab9c571a295b1ed47
Javran Cheng <Javran.C@gmail.com>
2020-09-30 23:46:07 -0700

    a bit less copy to do.

--- fifteen-solver/src/Game/Fifteen/Solvability.hs
+++ fifteen-solver/src/Game/Fifteen/Solvability.hs
@@ -16,9 +16,10 @@ import qualified Data.Vector.Mutable as VM
 
 -- Reference: https://www.cs.bham.ac.uk/~mdr/teaching/modules04/java2/TilesSolvability.html
 
-mergeSort :: forall a s. Ord a => V.Vector a -> WriterT (Sum Int) (ST s) (V.Vector a)
-mergeSort xs = do
-  v <- V.thaw (xs :: V.Vector a)
+mergeSortFromListN :: forall a s. Ord a => Int -> [a] -> WriterT (Sum Int) (ST s) (V.Vector a)
+mergeSortFromListN n xs = do
+  -- actually safe to do as this is the only function holding this `v` reference.
+  v <- V.unsafeThaw (V.fromListN n xs)
   tmp <- VM.unsafeNew (VM.length v)
   let sortAux l r =
         -- ranges are inclusive.
@@ -69,11 +70,10 @@ testMergeSort :: IO ()
 testMergeSort = do
   let src :: [Int]
       src = [27, 79, 25, 6, 33, 31, 95, 64, 29, 77, 23, 62, 16, 89, 20, 80, 54, 52, 60, 70, 12, 64, 1, 24, 30, 2, 37, 34, 61, 37, 94, 70, 86, 7, 26, 49, 1, 79, 27, 40, 65, 25, 39, 11, 34, 58, 95, 3, 80, 43, 13, 30, 41, 13, 42, 100, 65, 84, 69, 52, 29, 67, 11, 69, 96, 51, 32, 38, 48, 17, 71, 32, 88, 98, 82, 5, 86, 34, 76, 18, 87, 47, 60, 21, 90, 55, 74, 27, 41, 26, 39, 61, 12, 43, 26, 4, 54, 97, 80, 14]
-      xs = V.fromList src
-      (ys, count) = runST $ runWriterT $ mergeSort xs
+      (ys, count) = runST $ runWriterT $ mergeSortFromListN (length src) src
   print (V.toList ys == sort src)
   print count
   -- sanity check against example in https://www.cs.bham.ac.uk/~mdr/teaching/modules04/java2/TilesSolvability.html
-  let v = V.fromList @Int [12, 1, 10, 2, 7, 11, 4, 14, 5, 9, 15, 8, 13, 6, 3]
-      (_, count') = runST $ runWriterT $ mergeSort v
+  let v = [12 :: Int, 1, 10, 2, 7, 11, 4, 14, 5, 9, 15, 8, 13, 6, 3]
+      (_, count') = runST $ runWriterT $ mergeSortFromListN (length v) v
   print (count' == 49)
