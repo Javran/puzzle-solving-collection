67e07b2e0a806bdf627173829be34a096ad0d069
Javran Cheng <Javran.C@gmail.com>
2020-10-01 23:07:16 -0700

    convert into internal rep.

--- torus-solver/src/Game/Torus/Main.hs
+++ torus-solver/src/Game/Torus/Main.hs
@@ -1,9 +1,37 @@
+{-# LANGUAGE NamedFieldPuns #-}
+
 module Game.Torus.Main
   ( main
   )
 where
 
+import Control.Monad
+import qualified Data.Set as S
+import qualified Data.Vector as V
 import Game.Torus.Parser
 
+data Board = Board
+  { bdDims :: (Int, Int)
+  , -- row*col, row-major.
+    bdTiles :: V.Vector Int
+  }
+  deriving (Show)
+
+mkBoard :: BoardRep -> Maybe Board
+mkBoard (bdDims@(rows, cols), tiles) = do
+  let flat = concat tiles
+  guard $ length tiles == rows
+  guard $ all ((== cols) . length) tiles
+  guard $ S.fromList flat == S.fromList [1 .. rows * cols]
+  pure
+    Board
+      { bdDims
+      , bdTiles =
+          -- note that after verification
+          -- we have numbers shifted to be 0-based,
+          -- as it's a bit nicer to work with.
+          V.fromListN (rows * cols) $ fmap pred flat
+      }
+
 main :: IO ()
-main = print (parseBoard demo0)
+main = print (parseBoard demo0 >>= mkBoard)
