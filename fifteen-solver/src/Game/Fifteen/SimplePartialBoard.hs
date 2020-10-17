681d1d6fccc7eca5a09c510857d8f62d4d087ada
Javran Cheng <Javran.C@gmail.com>
2020-10-16 22:41:19 -0700

    first attempt at impl this.
    may or may not work. needs some testing.

--- fifteen-solver/src/Game/Fifteen/SimplePartialBoard.hs
+++ fifteen-solver/src/Game/Fifteen/SimplePartialBoard.hs
@@ -1,7 +1,8 @@
+{-# LANGUAGE MultiWayIf #-}
+
 module Game.Fifteen.SimplePartialBoard where
 
 import Game.Fifteen.Types
-
 
 {-
   A SimplePartialBoard is simply
@@ -16,3 +17,40 @@ import Game.Fifteen.Types
   making this move)
  -}
 type SimplePartialBoard = (Coord, Coord)
+
+makeMove :: SimplePartialBoard -> Coord -> Maybe SimplePartialBoard
+makeMove (curCoord@(cR, cC), holeCoord@(hR, hC)) mCoord@(mR, mC)
+  | mCoord == holeCoord =
+    -- invalid move if hole is already at that coord.
+    Nothing
+  | mR /= hR && mC /= hC =
+    -- move must share either row or col with the hole
+    Nothing
+  | mR == hR =
+    -- same row
+    if
+        | hC < cC ->
+          -- hole is on the left side of cur tile.
+          if mC < cC
+            then Just (curCoord, mCoord)
+            else Just ((cR, cC -1), mCoord)
+        | cC < hC ->
+          -- hole is on the right side of cur tile.
+          if cC < mC
+            then Just (curCoord, mCoord)
+            else Just ((cR, cC + 1), mCoord)
+        | otherwise -> error "unreachable"
+  | mC == hC =
+    if
+        | hR < cR ->
+          -- hole is on the up side of cur tile.
+          if mR < cR
+            then Just (curCoord, mCoord)
+            else Just ((cR -1, cC), mCoord)
+        | cR < hR ->
+          -- hole is on the right side of cur tile.
+          if cR < mR
+            then Just (curCoord, mCoord)
+            else Just ((cR + 1, cC), mCoord)
+        | otherwise -> error "unreachable"
+  | otherwise = error "unreachable"
