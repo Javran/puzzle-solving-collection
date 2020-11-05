6147ccfe51587a36f3b27ca682aa7f94d66d0431
Javran Cheng <Javran.C@gmail.com>
2020-11-04 23:47:31 -0800

    check tree-tent counts.

--- tents-solver/src/Game/Tents/Verify.hs
+++ tents-solver/src/Game/Tents/Verify.hs
@@ -1,7 +1,9 @@
 module Game.Tents.Verify where
 
+import Control.Monad
 import qualified Data.Map.Strict as M
 import Data.Maybe
+import qualified Data.Set as S
 import Game.Tents.Types
 
 {-
@@ -9,6 +11,22 @@ import Game.Tents.Types
  -}
 
 verifyBoard :: (Int, Int) -> M.Map Coord Cell -> Bool
-verifyBoard dims cells = isJust $ undefined
+verifyBoard dims cells = isJust $ do
+  let allCoords =
+        S.fromList
+          [ (r, c)
+          | r <- [0 .. rows -1]
+          , c <- [0 .. cols -1]
+          ]
+      (treeCoords, tentCoords) = foldMap go (M.toList cells)
+        where
+          go (x, Tent) = (S.empty, S.singleton x)
+          go (x, Tree) = (S.singleton x, S.empty)
+          go _ = (S.empty, S.empty)
+  -- make sure all coords are assigned Cell values.
+  guard $ allCoords == M.keysSet cells
+  -- make sure the count is correct to make pairs.
+  guard $ S.size treeCoords == S.size tentCoords
+  Nothing
   where
     (rows, cols) = dims
