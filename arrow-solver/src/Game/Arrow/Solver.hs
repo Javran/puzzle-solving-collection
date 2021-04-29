b7e8324038bdfeb7fbd80766928fe6e5906ceb8d
Javran Cheng <Javran.C@gmail.com>
2021-04-29 00:31:24 -0700

    playing with singletons a bit.
    kind level does not unify nicely,
    we'll need some wordarounds.

--- arrow-solver/src/Game/Arrow/Solver.hs
+++ arrow-solver/src/Game/Arrow/Solver.hs
@@ -1,4 +1,9 @@
+{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE KindSignatures #-}
 {-# LANGUAGE NamedFieldPuns #-}
+{-# LANGUAGE TypeApplications, ExistentialQuantification #-}
 
 module Game.Arrow.Solver
   (
@@ -6,6 +11,7 @@ module Game.Arrow.Solver
 where
 
 import Data.List.Split
+import Data.Proxy
 import Game.Arrow.CoordSystem
 import Game.Arrow.Gaussian
 import Game.Arrow.Types
@@ -16,8 +22,21 @@ hexSplit n = splitPlaces $ [n .. n + n -1] <> reverse (init splits)
   where
     splits = [n .. n + n -1]
 
+data SingPuzzleShape (s :: PuzzleShape) where
+  SHexagon :: SingPuzzleShape 'Hexagon
+  SSquare :: SingPuzzleShape 'Square
+
+class SingPuzzleShapeI (s :: PuzzleShape) where
+  sing :: SingPuzzleShape s
+
+instance SingPuzzleShapeI 'Hexagon where
+  sing = SHexagon
+
+instance SingPuzzleShapeI 'Square where
+  sing = SSquare
+
 solve :: Puzzle -> Either (Err Int) [[Int]]
-solve Puzzle {opMod, pzType = (Hexagon, sz), grid} = do
+solve Puzzle {opMod, pzType = (pzShape@Hexagon, sz), grid} = do
   let inp =
         (fmap . fmap)
           (\v -> (- v) `mod` opMod)
@@ -26,4 +45,4 @@ solve Puzzle {opMod, pzType = (Hexagon, sz), grid} = do
       mat = zipWith (\xs rhs -> foldr (:) [rhs] xs) matLhs (concat inp)
   case solveMatOne opMod mat of
     Left e -> Left e
-    Right xs -> Right $ hexSplit sz xs
+    Right xs -> Right $ toChunks (Proxy @'Hexagon) sz xs
