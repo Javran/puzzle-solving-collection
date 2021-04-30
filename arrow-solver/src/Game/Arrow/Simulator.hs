b594bb556b1f818f3238a40ba763667bc418e297
Javran Cheng <Javran.C@gmail.com>
2021-04-30 01:16:22 -0700

    implement a simulator.

---
+++ arrow-solver/src/Game/Arrow/Simulator.hs
@@ -0,0 +1,32 @@
+{-
+  Simulates moves performed on a puzzle.
+ -}
+{-# LANGUAGE NamedFieldPuns #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+
+module Game.Arrow.Simulator
+  ( applyMoves
+  )
+where
+
+import qualified Data.Map.Strict as M
+import Data.Monoid
+import Data.Proxy
+import Game.Arrow.CoordSystem
+import Game.Arrow.Types
+
+applyMoves :: Puzzle -> [[Int]] -> [[Int]]
+applyMoves Puzzle {opMod, pzType = (pzShape, sz), grid} shapedMoves =
+  withShape
+    pzShape
+    (\(pty :: Proxy k) ->
+       let (_, sCoords) = gCoords pty sz
+           moves :: M.Map (Coord k) Int
+           moves = M.fromList $ zip (concat sCoords) (concat shapedMoves)
+           initGridMap :: M.Map (Coord k) Int
+           initGridMap = M.fromList $ zip (concat sCoords) (concat grid)
+           getResult :: Coord k -> Int
+           getResult c = (initGridMap M.! c + moveCount) `mod` opMod
+             where
+               moveCount = getSum $ foldMap (Sum . (moves M.!)) (surrounding pty sz c)
+        in (fmap . fmap) getResult sCoords)
