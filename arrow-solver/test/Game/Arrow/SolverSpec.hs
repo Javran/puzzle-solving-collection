f5f1b13ce2ddc02a9ae35af2f59709df4a334f5f
Javran Cheng <Javran.C@gmail.com>
2021-04-30 02:25:56 -0700

    refactor.

--- arrow-solver/test/Game/Arrow/SolverSpec.hs
+++ arrow-solver/test/Game/Arrow/SolverSpec.hs
@@ -6,18 +6,26 @@ import Game.Arrow.Types
 import Test.Hspec
 
 spec :: Spec
-spec = describe "solve" $
-  specify "example" $ do
-    let xs =
-          [ [0, 1, 3, 4]
-          , [4, 4, 2, 1, 3]
-          , [0, 0, 2, 2, 1, 3]
-          , [4, 5, 1, 3, 1, 1, 5]
-          , [4, 0, 2, 4, 1, 5]
-          , [0, 3, 1, 0, 2]
-          , [3, 4, 5, 3]
-          ]
-        puzzle = Puzzle 6 (Hexagon, 4) xs
-        Right moves = solve puzzle
-    applyMoves puzzle moves
-      `shouldBe` (fmap . fmap) (const 0) xs
+spec = describe "solve" $ do
+  let solvable tag puzzle = specify tag $ do
+        let result = solve puzzle
+        case result of
+          Left err ->
+            expectationFailure $
+              "Expected puzzle to be solvable but got: " <> show err
+          Right moves ->
+            applyMoves puzzle moves
+              `shouldBe` (fmap . fmap) (const 0) (grid puzzle)
+  solvable
+    "example: mod 6, hexagon 4"
+    $ Puzzle
+      6
+      (Hexagon, 4)
+      [ [0, 1, 3, 4]
+      , [4, 4, 2, 1, 3]
+      , [0, 0, 2, 2, 1, 3]
+      , [4, 5, 1, 3, 1, 1, 5]
+      , [4, 0, 2, 4, 1, 5]
+      , [0, 3, 1, 0, 2]
+      , [3, 4, 5, 3]
+      ]
