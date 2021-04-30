b224f59155ce94f3be9bd2225a0b08ae8110875a
Javran Cheng <Javran.C@gmail.com>
2021-04-30 01:50:43 -0700

    to test by simulation.

--- arrow-solver/test/Game/Arrow/SolverSpec.hs
+++ arrow-solver/test/Game/Arrow/SolverSpec.hs
@@ -17,19 +17,7 @@ spec = describe "solve" $
           , [0, 3, 1, 0, 2]
           , [3, 4, 5, 3]
           ]
-    {-
-      TODO: simulate and verify if the result is correct rather
-      having to write out the solution
-      (this adds a bit of flexibility also as
-      this would allow alternative solutions as long as puzzle is solved.
-     -}
-    solve (Puzzle 6 (Hexagon, 4) xs)
-      `shouldBe` Right
-        [ [4, 1, 1, 3]
-        , [4, 3, 2, 2, 2]
-        , [3, 0, 0, 1, 0, 2]
-        , [5, 0, 2, 2, 3, 1, 4]
-        , [0, 3, 4, 3, 1, 0]
-        , [0, 3, 5, 2, 1]
-        , [0, 0, 0, 0]
-        ]
+        puzzle = Puzzle 6 (Hexagon, 4) xs
+        Right moves = solve puzzle
+    applyMoves puzzle moves
+      `shouldBe` (fmap . fmap) (const 0) xs
