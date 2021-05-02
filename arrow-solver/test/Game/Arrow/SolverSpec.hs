195f97699a10b327a04d4921b8b901ac3cbef325
Javran Cheng <Javran.C@gmail.com>
2021-05-01 23:47:51 -0700

    port more examples.

--- arrow-solver/test/Game/Arrow/SolverSpec.hs
+++ arrow-solver/test/Game/Arrow/SolverSpec.hs
@@ -17,7 +17,7 @@ spec = describe "solve" $ do
             applyMoves puzzle moves
               `shouldBe` (fmap . fmap) (const 0) (grid puzzle)
   solvable
-    "example: mod 4, square 3"
+    "example #0: mod 4, square 3"
     $ Puzzle
       4
       (Square, 3)
@@ -26,7 +26,7 @@ spec = describe "solve" $ do
       , [2, 2, 3]
       ]
   solvable
-    "example: mod 6, hexagon 4"
+    "example #1: mod 6, hexagon 4"
     $ Puzzle
       6
       (Hexagon, 4)
@@ -37,4 +37,27 @@ spec = describe "solve" $ do
       , [4, 0, 2, 4, 1, 5]
       , [0, 3, 1, 0, 2]
       , [3, 4, 5, 3]
+      ]
+  solvable
+    "example #2: mod 6, hexagon 4"
+    $ Puzzle
+      6
+      (Hexagon, 4)
+      [ [4, 1, 2, 1]
+      , [2, 1, 5, 3, 0]
+      , [4, 4, 4, 2, 2, 3]
+      , [4, 4, 2, 0, 0, 2, 2]
+      , [5, 1, 5, 4, 2, 5]
+      , [2, 3, 3, 5, 1]
+      , [3, 4, 5, 3]
+      ]
+  solvable
+    "example #3: mod 4, square 4"
+    $ Puzzle
+      4
+      (Square, 4)
+      [ [2, 2, 3, 3]
+      , [3, 2, 3, 3]
+      , [1, 0, 3, 0]
+      , [3, 2, 2, 0]
       ]
